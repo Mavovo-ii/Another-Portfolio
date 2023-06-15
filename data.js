35ca761c418ff1159906179e66e1c4c28cae8bf0
Mavovo-ii <96288027+Mavovo-ii@users.noreply.github.com>
2023-06-15 12:55:29 +0200

    Add files via upload

---
+++ data.js
@@ -0,0 +1,28 @@
+export const info = [
+  {
+    name: "Personal Dashboard",
+    id: 1,
+    concept:
+      "I built this project using JavaScript, HTML and CSS, I used some APIs to fetch data, Unsplash API, DodgeCoin API and the OpenWeatherMap API. Planning to deploy it as a Chrome Extension in Chrome Web Store in the near future.",
+    image: "/FavExtPic.png",
+    link: "https://best-chrome-ext.netlify.app",
+  },
+
+  {
+    name: "e-Education Website",
+    id: 2,
+    concept:
+      "With the forth industrial revolution taking its toll, online education has prevailed. This is just another kind of an online school that teaches people to code building projects, this was built with JavaScript, HTML and CSS.",
+    image: "/EduWebPic.png",
+    link: "https://mavovo-coding.netlify.app",
+  },
+
+  {
+    name: "NFT Product Page",
+    id: 3,
+    concept:
+      "I built this one using HTML and CSS, As simple as it looks, it taught me a lot about responsive design and layout skills.",
+    image: "/GoingBack.png",
+    link: "https://mavovo-Nftee.netlify.app",
+  },
+];
